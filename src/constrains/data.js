f92a219077a66646a89ee9f592f36079bf27e1bc
Muhabat Khan Niazi <muhabatkhanx@gmail.com>
2022-08-18 13:20:36 -0700

    project setup

---
+++ src/constrains/data.js
@@ -0,0 +1,20 @@
+const projects = [
+    {
+       title : 'Website  Design',
+        describe : 'Home is a Property buy and selling platform, where owner can sell their property and buyer reache out him.',
+    },
+    {
+        title : 'Mobile App  Design',
+         describe : 'Running daily is a fitness app where you can easliy calculate your calories count and burn more fat ',
+     },
+     {
+        title : 'Brand  Design',
+         describe : 'Dalson oil is one of the best oil to protect your  Demage skin for face and sclap and also reduce your hair fall',
+     },
+     {
+        title : 'Social media  Design',
+         describe : 'We create Social Media Marketing poster for Facebook, Instagram , twitter and others to increase your customer and sales',
+     }
+]
+
+export default projects;
